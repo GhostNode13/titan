3ccb01055ec9588c7d57b5f7301506d1dbda9e7e
GhostNode13 <shulika.nikita2015@gmail.com>
2025-02-12 23:41:48 +0300

    Logo

---
+++ logo.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Определяем цвета
+GREEN='\033[32m'
+RED='\033[31m'
+RESET='\033[0m'
+
+# Выводим слово GHOST с цветами
+echo -e "${GREEN}
+ GGGGG  H   H  OOO   SSSS  TTTTT
+G       H   H O   O  S       T
+G  GG   HHHHH O   O  SSSS    T
+G   G   H   H O   O     S    T
+ GGGG   H   H  OOO   SSSS    T
+${RESET}"
